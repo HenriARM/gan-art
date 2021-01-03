b0b847129b606a61769165b65d7c259b8dd4ec25
HenriARM <henriarm@yandex.ru>
2021-01-03 02:24:53 +0200

    Upd abstract dcgan

--- abstract-dcgan.py
+++ abstract-dcgan.py
@@ -11,18 +11,6 @@ import os
 import time
 from IPython import display
 
-# (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-# train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-# # Normalize the images to [-1, 1]
-# train_images = (train_images - 128) / 128
-#
-# BUFFER_SIZE = 60000
-# BATCH_SIZE = 256
-# ce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-#
-# # Batch and shuffle the data
-# train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-
 
 BATCH_SIZE = 32
 ce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
