e1d6d3397d565d19b8206b98333f13c08372b546
HenriARM <henriarm@yandex.ru>
2021-01-05 23:25:32 +0200

    Upd WikiArt scripts

--- image-transformations.py
+++ image-transformations.py
@@ -1,60 +1,60 @@
-# import numpy as np
 import os
 from PIL import Image
 from os import listdir
 import Augmentor
 
+from_path = './datasets/abstract'
+to_path = './ds'
+
 
 def resize_plus_change_type():
-    from_path = './dataset'
-    to_path = './dataset-transformed-28'
     for file in listdir(from_path):
         image = Image.open(from_path + '/' + file)
         if image.format == 'JPEG' or image.format == 'JPG':
-            image_resized = image.resize((28, 28), resample=Image.BILINEAR)
+            image_resized = image.resize((128, 128), resample=Image.BILINEAR)
             image_resized.save(to_path + '/' + os.path.splitext(file)[0] + '.png')
         else:
             pass
             # TODO: print name of image and check formats
 
 
-def main():
-    p = Augmentor.Pipeline('./dataset-transformed')
+def apply_transformations():
+    p = Augmentor.Pipeline(to_path)
     p.rotate90(probability=1)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.rotate180(probability=1)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.rotate270(probability=1)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.zoom(probability=1, min_factor=1.1, max_factor=1.5)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=8)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.skew_tilt(probability=1)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.shear(probability=1, max_shear_left=0.20, max_shear_right=0.20)
     p.status()
     p.process()
 
-    p = Augmentor.Pipeline('./dataset-transformed')
+    p = Augmentor.Pipeline(to_path)
     p.resize(probability=1, width=256, height=256)
     p.crop_random(probability=1, percentage_area=0.5)
     p.status()
@@ -63,4 +63,4 @@ def main():
 
 if __name__ == '__main__':
     # resize_plus_change_type()
-    main()
+    apply_transformations()
--- wikiart.py
+++ wikiart.py
@@ -1,32 +1,37 @@
 # WikiArt API is very poor. Pagination doesn't work and most of links are broken.
 import requests
+
 # TODO: code can be rewritten using API 2, but it has same endopoints + Token Auth will be needed
 
 # Get all Artists by specific Genre
-genre = 'abstract' # seo_name in API
+# genre = 'abstract' # seo_name in API
+genre = 'flower-painting'  # seo_name in API
 page = 1
 page_size = 600
-url_get_artists_by_genre = 'http://www.wikiart.org/en/Artists-by-Genre/{genre}/{page}?json=1&pagesize={page_size}'\
+url_get_artists_by_genre = 'http://www.wikiart.org/en/Artists-by-Genre/{genre}/{page}?json=1&pagesize={page_size}' \
     .format(genre=genre, page=page, page_size=page_size)
 # approxim 500 abstract genre authors
 artists = requests.get(url_get_artists_by_genre).json()
+# artists = artists[::-1]
 
 count = 0
 for artist in artists:
     # Loop through Artists and get all Paintings by that Artist
     artist_url = artist['url']
-    url_get_paintings_by_artist = 'https://www.wikiart.org/en/App/Painting/PaintingsByArtist?artistUrl={artist_url}&json=1'\
+    url_get_paintings_by_artist = 'https://www.wikiart.org/en/App/Painting/PaintingsByArtist?artistUrl={artist_url}&json=1' \
         .format(artist_url=artist_url)
     paintings = requests.get(url_get_paintings_by_artist).json()
     for painting in paintings:
         # Get full information about painting,
         # since not all paintings from specific genre artist are same genre.
         content_id = painting['contentId']
-        url_get_painting_info = 'https://www.wikiart.org/en/App/Painting/ImageJson/{content_id}'\
+        url_get_painting_info = 'https://www.wikiart.org/en/App/Painting/ImageJson/{content_id}' \
             .format(content_id=content_id)
         painting_info = requests.get(url_get_painting_info).json()
         # check genre
-        if painting_info['genre'] != genre:
+        if painting_info is None or painting_info['genre'] is None:
+            continue
+        if len(painting_info['genre']) != len(genre):
             continue
         # Save image in FS
         painting_url = painting_info['image']
@@ -40,7 +45,8 @@ for artist in artists:
         with open('./dataset/' + painting_name, 'wb') as handler:
             handler.write(painting_bytes)
         count += 1
-        if count == 1000:
+        print(count)
+        if count == 2000:
             exit(1)
 
 # TODO: add photo transformations
