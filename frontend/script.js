a6da13cfe7698fab149eb1f43615aef3eb4a52ee
HenriARM <henriarm@yandex.ru>
2021-01-14 02:58:04 +0200

    Frontend: Added all basic functionality

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,27 @@
+/**
+ * @param {String} str - HTML representing a single element
+ * @return {Node | null}
+ */
+function htmlToElement(str) {
+    let template = document.createElement('template');
+    str = str.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = str;
+    return template.content.firstChild;
+}
+
+/**
+ * @param {String} str - HTML representing any number of sibling elements
+ * @return {NodeList}
+ */
+function htmlToElements(str) {
+    let template = document.createElement('template');
+    template.innerHTML = str;
+    return template.content.childNodes;
+}
+
+
+//---------------------------- TODO: maybe separate help file
+
 let HttpClient = function () {
     this.get = function (aUrl, aCallback) {
         let asyncHttpRequest = new XMLHttpRequest();
@@ -11,20 +35,91 @@ let HttpClient = function () {
     }
 };
 
-function connectMinio() {
-
+function createImageListItem(imageName, imageLink) {
+    return htmlToElements('<li>' +
+        '<input type="radio" name="select" id="' + imageName + '">' +
+        '<div class="item-hugger">' +
+        '<div class="title">' + imageName + '</div>' +
+        '<img class="thumb-image" src="' + imageLink + '"/>' +
+        '<label for="' + imageName + '"></label>' +
+        '</div>' +
+        '<div class="content">' +
+        '<div class="item-wrapper">' +
+        '<img src="' + imageLink + '"/>' +
+        '<div class="title">' + imageName + '</div>' +
+        '</div>' +
+        '</div>' +
+        '</li>');
 }
 
-function doSmth() {
-    console.log('Hello World');
+function clearGallery() {
+    let galleryList = document.getElementById("gallery");
+    // delete all child elements
+    galleryList.innerHTML = '';
+}
+
+function generateGallery() {
+
+    // Get nodes which store form data
+    const datasetType = document.getElementById("form-dataset-type").value;
+    const gallerySize = document.getElementById("form-gallery-size").value;
+
+    console.log(datasetType);
+    console.log(gallerySize);
+
+    if (datasetType === undefined || datasetType === null ||
+        gallerySize === undefined || gallerySize === null) {
+        console.log("error");
+        // TODO
+    }
+
+    // check gallerySize is positive number
+    if (gallerySize <= 0) {
+        console.log("bad gallery size");
+    }
+
+
+    // build request
+    const request = "http://0.0.0.0:4444/images?dataset=" + datasetType + "&size=" + gallerySize;
+    console.log(request);
 
     let client = new HttpClient();
-    client.get('http://0.0.0.0:4444/images/?size=64', function (response) {
-        let image = new Image();
-        image.src = 'data:image/png;base64,' + response;
-        document.body.appendChild(image);
+    client.get(request, function (response) {
+        console.log(response);
+        console.log(typeof response);
+
+        let imagePayload = JSON.parse(response);
+        console.log(imagePayload);
+        console.log(typeof imagePayload);
+
+        let galleryList = document.getElementById("gallery");
+
+        for (let i = 0; i < imagePayload.length; i++) {
+            let tuple = imagePayload[i];
+            let imageName = tuple[0];
+            let imageLink = tuple[1];
+
+            console.log(imageName);
+            console.log(imageLink);
+
+            let imageItem = createImageListItem(imageName, imageLink);
+
+            for (let j = 0; j < imageItem.length; j++) {
+                galleryList.appendChild(imageItem[j]);
+
+            }
+            console.log(imageItem);
+            console.log(typeof imageItem);
+
+            // append image item to gallery
+
+        }
     });
+
 }
 
 
-
+function generate() {
+    clearGallery();
+    generateGallery();
+}
